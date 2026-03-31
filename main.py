ca67351e0a2007f232c87fde6905d3b16a97941e
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-03-31 13:54:29 +0530

    create class main and it's construstor in which the set instance variable and set title of window

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 from tkinter import Tk
 import random
+
+class main:
+    def __init__(self, root):
+        self.root = root
+        
+        # set title to the window
+        self.root.title(text="Game of Rock Paper Scissor")
+        
+        
